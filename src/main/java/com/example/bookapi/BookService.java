f58279d4190e0bc15e8c77d60e539f0e4075681a
unknown <kondratyev.sava@mail.ru>
2024-12-19 17:52:44 +0300

    Adding BookService.java

---
+++ src/main/java/com/example/bookapi/BookService.java
@@ -0,0 +1,34 @@
+package com.example.bookapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List&lt;Book&gt; getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional&lt;Book&gt; getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
